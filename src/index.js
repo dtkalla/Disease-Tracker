21be8e7231acfafb5c14d188d01f2b015f6af40e
Daniel <danieltkalla@gmail.com>
2022-11-12 13:01:16 -0500

    convert country codes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import Example from "./scripts/example";
 const fetch = require('node-fetch');
+// const XML = require('xhr2');
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
     const main = document.getElementById("main");
     new Example(main);
 });
+
--- src/scripts/dummy_test_file.js
+++ src/scripts/dummy_test_file.js
@@ -1,21 +1,20 @@
 const fetch = require('node-fetch');
+const conversionHash = require("./conversion.js");
 let url = 'https://ghoapi.azureedge.net/api/MALARIA_EST_DEATHS?$filter=SpatialDimType%20eq%20%27COUNTRY%27and%20TimeDim%20eq%202020';
+let url2 = 'https://ghoapi.azureedge.net/api/DIMENSION/COUNTRY/DimensionValues'
 
-async function getData() {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw response;
-    }
-    const data = await response.json();
+// async function getData() {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//         throw response;
+//     }
+//     const data = await response.json();
     
-    // console.log(data["value"][0]);
+//     // console.log(data["value"][0]);
     
-    return data
-}
-
-
-
-getData()
+//     return data
+// }
+// getData()
 
 
 
@@ -35,13 +34,15 @@ function arrayTable(data) {
     const table = [];
     for (let i = 0; i < data.length; i++) {
         if (data[i].NumericValue === null) {
-            table.push([0,data[i].SpatialDim])
+            // table.push([conversionHash[data[i].SpatialDim],0])
         } else {
-            table.push([parseInt(data[i].NumericValue),data[i].SpatialDim])
+        table.push([conversionHash[data[i].SpatialDim],data[i].NumericValue])
         }
     }
     console.log(table)
+    
 }
+
 
 
 // const req = new XMLHttpRequest();
@@ -71,3 +72,31 @@ function arrayTable(data) {
 // console.log(dataTable())
 
 printArray()
+
+
+
+
+
+// function makeConversion() {
+//     fetch(url).then(response => {
+//         if (!response.ok) {
+//           throw new Error('Network response was not OK');
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         arrayTable(data["value"]);
+//     })
+// }
+
+// function arrayTable(data) {
+//     const table = [];
+//     for (let i = 0; i < data.length; i++) {
+//         if (data[i].NumericValue === null) {
+//             table.push([0,data[i].SpatialDim])
+//         } else {
+//             table.push([parseInt(data[i].NumericValue),data[i].SpatialDim])
+//         }
+//     }
+//     console.log(table)
+// }
